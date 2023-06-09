a05812bb7f3446042872b8f6c75cec8c264488a1
Igor Molchanov <66907532+meg4cyberc4t@users.noreply.github.com>
2023-06-09 19:46:36 +0300

    Initial Commit

---
+++ SwiftNavigationExample/SwiftNavigationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNavigationExampleApp.swift
+//  SwiftNavigationExample
+//
+//  Created by Igor Molchanov on 09.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNavigationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
